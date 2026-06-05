0a226769cf04252da1effe37a13c735406d8ce1d
= <=>
2026-06-05 10:48:05 +0100

    removed django-browser-reload from the settings.py file as it was preventing the page from loading. Don't know how it got there tbh

--- config/settings.py
+++ config/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django_browser_reload.middleware.BrowserReloadMiddleware",
     # "business-case-improvemnt-app.middleware.HealthCheckMiddleware"
 ]
 
